f49d75eff0887fbd21b27f084266331e6dcccbe0
aungthureintun <aungthurein163@gmail.com>
2024-07-21 02:19:04 +0630

    add arbitarymethodreference

--- api-04-lambda/src/main/java/com/jdc/lambda/effective/EffectiveFinalDemo.java
+++ api-04-lambda/src/main/java/com/jdc/lambda/effective/EffectiveFinalDemo.java
@@ -5,14 +5,19 @@ import java.util.List;
 
 public class EffectiveFinalDemo {
 	
+
+	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<Integer>();
+		
+		Runnable runnable = () -> {
+			list.forEach(data -> System.out.println(data));
+			list.forEach(System.out::println);
+		};
 	
-	Runnable runnable = () -> {
-		list.forEach(data -> System.out.println(data));
-		list.add(null);
-	};
 	
 
+	}
+		
 	}
 
 		
