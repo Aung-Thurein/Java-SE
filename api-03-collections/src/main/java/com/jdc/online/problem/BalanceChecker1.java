35853db1331aa6d66cc1e2f5f71923f148ef4e0a
aungthureintun <aungthurein163@gmail.com>
2024-07-21 02:39:52 +0630

    add BalanceChecker

--- api-03-collections/src/main/java/com/jdc/online/problem/BalanceChecker1.java
+++ api-03-collections/src/main/java/com/jdc/online/problem/BalanceChecker1.java
@@ -1,6 +1,46 @@
 package com.jdc.online.problem;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BalanceChecker1 {
 	
+	public boolean isBalance(String value) 
+	{
+		
+		if(value ==null || value.isBlank())
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		char[] chars = value.toCharArray();
+		Queue<Character> stack = Collections.asLifoQueue(new ArrayDeque<Character>());
+		
+		for(char current :chars)
+		{
+			switch(current)
+			{
+			case '(','{','[' -> stack.offer(current);
+			case ')','}',']' -> {
+				var last = stack.poll();
+				if(null == last || !isMatch(last, current))
+				{
+					return false;
+				}
+			}
+			default -> throw new IllegalArgumentException();
+			}
+		}
+
+		return stack.isEmpty();
+	}
 	
+	public boolean isMatch(Character last, char current)
+	{
+		return (last == '(' && current == ')')
+			|| (last == '{' && current == '}')
+			|| (last == '[' && current == ']');
+	}
 }
